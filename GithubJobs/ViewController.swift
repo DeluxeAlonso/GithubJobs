116855fccba645ed015cfeb465276c75042c37c8
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-11-07 08:21:57 -0500

    First commit

---
+++ GithubJobs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubJobs
+//
+//  Created by Alonso on 11/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
