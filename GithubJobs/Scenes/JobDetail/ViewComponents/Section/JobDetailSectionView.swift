2f0fc405db98a67e2da4a45707fe5310159ff6c5
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-05-18 23:57:47 -0500

    Updates section header label text color

--- GithubJobs/Scenes/JobDetail/ViewComponents/Section/JobDetailSectionView.swift
+++ GithubJobs/Scenes/JobDetail/ViewComponents/Section/JobDetailSectionView.swift
@@ -14,6 +14,7 @@ class JobDetailSectionView: UIView {
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
+        label.textColor = .black
         label.backgroundColor = .systemTeal
 
         return label
