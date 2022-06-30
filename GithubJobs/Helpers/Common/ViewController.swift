e198bf505bd7bf6503c0c566f81a934a573ca487
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-06-29 23:38:46 -0500

    Implements base view controller that handles theme manager

---
+++ GithubJobs/Helpers/Common/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  GithubJobs
+//
+//  Created by Alonso on 29/06/22.
+//
+
+import UIKit
+import Combine
+
+class ViewController: UIViewController {
+
+    private let themeManager: ThemeManagerProtocol
+
+    private var themeCancellable: Set<AnyCancellable> = []
+
+    init(themeManager: ThemeManagerProtocol) {
+        self.themeManager = themeManager
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        themeManager.interfaceStyle
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] userInterfaceStyle in
+                guard let self = self else { return }
+                self.overrideUserInterfaceStyle = userInterfaceStyle
+            }.store(in: &themeCancellable)
+    }
+
+}
