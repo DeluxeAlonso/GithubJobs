9fd62230c6bdc21a642a8a4634ce38a4d05ba474
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-07-24 00:18:37 -0500

    Adds initial implementation for settings scene

--- GithubJobs/Scenes/Settings/SettingsCoordinator.swift
+++ GithubJobs/Scenes/Settings/SettingsCoordinator.swift
@@ -5,4 +5,38 @@
 //  Created by Alonso on 24/07/22.
 //
 
-import Foundation
+import UIKit
+
+final class SettingsCoordinator: NSObject, Coordinator, SettingsCoordinatorProtocol {
+
+    let navigationController: UINavigationController
+
+    var childCoordinators: [Coordinator] = []
+    var parentCoordinator: Coordinator?
+    var presentingViewController: UIViewController?
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let themeManager = ThemeManager.shared
+        let viewModel = ThemeSelectionViewModel(themeManager: themeManager)
+        let viewController = ThemeSelectionViewController(themeManager: themeManager,
+                                                          viewModel: viewModel,
+                                                          coordinator: self)
+
+        navigationController.pushViewController(viewController, animated: false)
+        navigationController.modalPresentationStyle = .fullScreen
+
+        presentingViewController?.present(navigationController, animated: true, completion: nil)
+    }
+
+    func dismiss() {
+        let presentedViewController = navigationController.topViewController
+        presentedViewController?.dismiss(animated: true) { [weak self] in
+            self?.parentCoordinator?.childDidFinish()
+        }
+    }
+
+}
--- GithubJobs/Scenes/Settings/SettingsProtocols.swift
+++ GithubJobs/Scenes/Settings/SettingsProtocols.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+protocol SettingsViewModelProtocol {}
+
+protocol SettingsCoordinatorProtocol: AnyObject {}
