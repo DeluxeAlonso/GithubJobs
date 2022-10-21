3252b7c7e847d6e2a0ee836658f5eee42ccbe86b
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-10-20 23:03:45 -0500

    Implements Fade method in Animator class

--- GithubJobs/Helpers/ViewComponents/Animations/Animator.swift
+++ GithubJobs/Helpers/ViewComponents/Animations/Animator.swift
@@ -5,4 +5,15 @@
 //  Created by Alonso on 20/10/22.
 //
 
-import Foundation
+import UIKit
+
+class Animator {
+
+    class func fade(view: UIView) {
+        view.layer.opacity = 0.1
+        UIView.animateKeyframes(withDuration: 0.5, delay: 0.0, options: .allowUserInteraction, animations: {
+            view.layer.opacity = 1
+        }, completion: nil)
+    }
+
+}
