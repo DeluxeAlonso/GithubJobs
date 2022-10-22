dde6cfd2b8363159352b9d0de1f56cfa27fe8c23
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-10-21 23:27:33 -0500

    Updates Animator methods

--- GithubJobs/Helpers/ViewComponents/Animations/Animator.swift
+++ GithubJobs/Helpers/ViewComponents/Animations/Animator.swift
@@ -9,19 +9,19 @@ import UIKit
 
 class Animator {
 
-    class func fade(view: UIView) {
+    class func fade(view: UIView, completion: ((Bool) -> Void)? = nil) {
         view.layer.opacity = 0.1
         UIView.animateKeyframes(withDuration: 0.5, delay: 0.0, options: .allowUserInteraction, animations: {
             view.layer.opacity = 1
-        }, completion: nil)
+        }, completion: completion)
     }
 
-    class func fade(tableViewCell: UITableViewCell) {
+    class func fade(tableViewCell: UITableViewCell, completion: ((Bool) -> Void)? = nil) {
         let view = tableViewCell.contentView
         view.layer.opacity = 0.1
         UIView.animateKeyframes(withDuration: 0.5, delay: 0.0, options: .allowUserInteraction, animations: {
             view.layer.opacity = 1
-        }, completion: nil)
+        }, completion: completion)
     }
 
 }
