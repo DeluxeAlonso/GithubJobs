3ae072fdb5cec68301f4bbd29b817a1320403fb9
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-10-23 00:49:13 -0500

    Refactors fade methods in Animator class

--- GithubJobs/Helpers/ViewComponents/Animations/Animator.swift
+++ GithubJobs/Helpers/ViewComponents/Animations/Animator.swift
@@ -17,19 +17,11 @@ class Animator {
     }
 
     class func fade(tableViewCell: UITableViewCell, completion: ((Bool) -> Void)? = nil) {
-        let view = tableViewCell.contentView
-        view.layer.opacity = 0.1
-        UIView.animateKeyframes(withDuration: 0.5, delay: 0.0, options: .allowUserInteraction, animations: {
-            view.layer.opacity = 1
-        }, completion: completion)
+        fade(view: tableViewCell.contentView, completion: completion)
     }
 
     class func fade(collectionViewCell: UICollectionViewCell, completion: ((Bool) -> Void)? = nil) {
-        let view = collectionViewCell.contentView
-        view.layer.opacity = 0.1
-        UIView.animateKeyframes(withDuration: 0.5, delay: 0.0, options: .allowUserInteraction, animations: {
-            view.layer.opacity = 1
-        }, completion: completion)
+        fade(view: collectionViewCell.contentView, completion: completion)
     }
 
 }
