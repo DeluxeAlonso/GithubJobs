542b3622ce87e70b61eb6a0b6dc1788a6119fa61
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-11-14 01:27:46 -0500

    Creates extension for Coordinator protocol

--- GithubJobs/Helpers/Protocols/Coordinator.swift
+++ GithubJobs/Helpers/Protocols/Coordinator.swift
@@ -37,3 +37,19 @@ extension Coordinator {
     }
 
 }
+
+extension Coordinator where Self: UINavigationControllerDelegate {
+
+    func finishChildOnPopIfneeded() {
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+            return
+        }
+        // Check whether our view controller array already contains that view controller.
+        // If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
+        if navigationController.viewControllers.contains(fromViewController) {
+            return
+        }
+        unwrappedParentCoordinator.childDidFinish()
+    }
+
+}
