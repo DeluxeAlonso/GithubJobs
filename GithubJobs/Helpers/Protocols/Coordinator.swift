c057775d39b72c33a8277e6d9583746ddfb9bfcd
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-11-15 21:37:57 -0500

    Implements BaseCoordinator class

--- GithubJobs/Helpers/Protocols/Coordinator.swift
+++ GithubJobs/Helpers/Protocols/Coordinator.swift
@@ -53,3 +53,31 @@ extension Coordinator where Self: UINavigationControllerDelegate {
     }
 
 }
+
+class BaseCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
+
+    var childCoordinators: [Coordinator] = []
+    var parentCoordinator: Coordinator?
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        fatalError("Start method should be implemented")
+    }
+
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+            return
+        }
+        // Check whether our view controller array already contains that view controller.
+        // If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
+        if navigationController.viewControllers.contains(fromViewController) {
+            return
+        }
+        unwrappedParentCoordinator.childDidFinish()
+    }
+
+}
