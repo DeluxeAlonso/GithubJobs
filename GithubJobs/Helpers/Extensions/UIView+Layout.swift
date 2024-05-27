2a0c1be0f157579f86aa8e87d703edad741b85c3
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-05-26 23:48:37 -0500

    Updates UIView+Layout

--- GithubJobs/Helpers/Extensions/UIView+Layout.swift
+++ GithubJobs/Helpers/Extensions/UIView+Layout.swift
@@ -10,29 +10,32 @@ import UIKit
 extension UIView {
 
     @discardableResult
-    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?,
-                bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?,
-                padding: UIEdgeInsets = .zero, size: CGSize? = nil) -> AnchoredConstraints {
+    func anchor(top: NSLayoutYAxisAnchor?,
+                leading: NSLayoutXAxisAnchor?,
+                bottom: NSLayoutYAxisAnchor?,
+                trailing: NSLayoutXAxisAnchor?,
+                padding: UIEdgeInsets = .zero,
+                size: CGSize? = nil) -> AnchoredConstraints {
         translatesAutoresizingMaskIntoConstraints = false
         var anchoredConstraints = AnchoredConstraints()
 
-        if let top = top {
+        if let top {
             anchoredConstraints.top = topAnchor.constraint(equalTo: top, constant: padding.top)
         }
 
-        if let leading = leading {
+        if let leading {
             anchoredConstraints.leading = leadingAnchor.constraint(equalTo: leading, constant: padding.left)
         }
 
-        if let bottom = bottom {
+        if let bottom {
             anchoredConstraints.bottom = bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom)
         }
 
-        if let trailing = trailing {
+        if let trailing {
             anchoredConstraints.trailing = trailingAnchor.constraint(equalTo: trailing, constant: -padding.right)
         }
 
-        if let size = size {
+        if let size {
             anchoredConstraints.width = widthAnchor.constraint(equalToConstant: size.width)
             anchoredConstraints.height = heightAnchor.constraint(equalToConstant: size.height)
         }
