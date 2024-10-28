38f689644afe822c0404ef31331048ef3806a62a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-10-27 23:33:46 -0500

    Creates Color selection scene

---
+++ GithubJobs/Scenes/Settings/ColorSelection/ColorSelectionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ColorSelectionViewModel.swift
+//  GithubJobs
+//
+//  Created by Alonso on 27/10/24.
+//
+
+import Combine
+
+final class ColorSelectionViewModel: ObservableObject {
+    
+}
