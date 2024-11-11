6b3cfb0783581a4371893d8a2601b127ba6f726c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-11-10 23:46:15 -0500

    Implements ExpandCollapseControlDisclosureStyle

--- GithubJobs/Helpers/ViewComponents/ExpandCollapseControl/ExpandCollapseControlDisclosureStyle.swift
+++ GithubJobs/Helpers/ViewComponents/ExpandCollapseControl/ExpandCollapseControlDisclosureStyle.swift
@@ -8,7 +8,26 @@
 import SwiftUI
 
 struct ExpandCollapseControlDisclosureStyle: DisclosureGroupStyle {
-    
 
+    let expandCollapseControlConfiguration: ExpandCollapseControlConfiguration
+
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: .leading, spacing: 0.0) {
+            configuration.label
+            Spacer()
+            Image(uiImage: .checkmark)
+                .resizable()
+                .renderingMode(.template)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            withAnimation {
+                configuration.isExpanded.toggle()
+            }
+        }
+        if configuration.isExpanded {
+            configuration.content
+        }
+    }
 
 }
