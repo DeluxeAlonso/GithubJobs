4bff0214c64ffc973e2df0829fd024867cd86bf3
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-11-25 00:18:08 -0500

    Implements SettingsInteractor

--- GithubJobs/Scenes/Settings/SettingsInteractor.swift
+++ GithubJobs/Scenes/Settings/SettingsInteractor.swift
@@ -5,8 +5,25 @@
 //  Created by Alonso on 23/11/24.
 //
 
-import Foundation
+import UIKit
 
 final class SettingsInteractor: SettingsInteractorProtocol {
-    
+
+    private let themeManager: ThemeManagerProtocol
+    private let featureFlagsManager: FeatureFlagsManagerProtocol
+
+    init(themeManager: ThemeManagerProtocol,
+         featureFlagsManager: FeatureFlagsManagerProtocol) {
+        self.themeManager = themeManager
+        self.featureFlagsManager = featureFlagsManager
+    }
+
+    func getFeatureFlagValue(for identifier: FeatureFlagIdentifier) async -> Bool {
+        await featureFlagsManager.value(for: identifier)
+    }
+
+    func getCurrentInterfaceStyle() async -> UIUserInterfaceStyle {
+        themeManager.interfaceStyle.value
+    }
+
 }
