8fb305b3cba9ee3809c69d739d42b51ff627beea
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-11-27 08:15:51 -0500

    Adds unit tests for SettingsViewModel load items method

--- GithubJobsTests/SettingsViewModelTests.swift
+++ GithubJobsTests/SettingsViewModelTests.swift
@@ -6,30 +6,64 @@
 //
 
 import XCTest
+import Combine
+@testable import GithubJobs
 
-final class SettingsViewModelTests: XCTestCase {
+class SettingsViewModelTests: XCTestCase {
+
+    private var mockInteractor: MockSettingsInteractor!
+    private var viewModel: SettingsViewModel!
+
+    private var cancellables: Set<AnyCancellable> = []
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        mockInteractor = MockSettingsInteractor()
+        viewModel = SettingsViewModel(interactor: mockInteractor)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockInteractor = nil
+        viewModel = nil
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testLoadItems() {
+        // Arrange
+        let expectation = XCTestExpectation(description: "Should receive items")
+        // Act
+        viewModel.itemModelsPublisher
+            .dropFirst()
+            .sink { items in
+                XCTAssertEqual(items.count, 2)
+                XCTAssertEqual(items.first?.title, "Themes")
+                XCTAssertEqual(items.last?.title, "Feature Flags")
+                expectation.fulfill()
         }
+        .store(in: &cancellables)
+        viewModel.loadItems()
+        // Assert
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testLoadItemsWithFAQs() {
+        // Arrange
+        let expectation = XCTestExpectation(description: "Should receive items")
+        mockInteractor.getFeatureFlagValueResult = true
+        // Act
+        viewModel.itemModelsPublisher
+            .dropFirst()
+            .sink { items in
+                XCTAssertEqual(items.count, 3)
+                XCTAssertEqual(items.first?.title, "Themes")
+                XCTAssertEqual(items[1].title, "FAQs")
+                XCTAssertEqual(items.last?.title, "Feature Flags")
+                expectation.fulfill()
+        }
+        .store(in: &cancellables)
+        viewModel.loadItems()
+        // Assert
+        wait(for: [expectation], timeout: 1.0)
     }
 
 }
