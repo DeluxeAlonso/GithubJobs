4bbb85d6d7df5057cea3895c0e11834e0715c6a6
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-12-02 23:34:47 -0500

    Implements FAQ entity

---
+++ GithubJobs/Entities/FAQ.swift
@@ -0,0 +1,16 @@
+//
+//  FAQ.swift
+//  GithubJobs
+//
+//  Created by Alonso on 2/12/24.
+//
+
+import Foundation
+
+struct FAQ: Decodable {
+
+    let id: String
+    let title: String
+    let description: String
+
+}
---
+++ GithubJobs/Networking/FeedResults/FAQsResult.swift
@@ -0,0 +1,8 @@
+//
+//  FAQsResult.swift
+//  GithubJobs
+//
+//  Created by Alonso on 2/12/24.
+//
+
+import Foundation
