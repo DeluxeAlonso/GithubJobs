353269c5ba76657064d3ec57a99282bb7cc85c60
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-12-04 23:49:45 -0500

    Implements FAQsResult

--- GithubJobs/Networking/FeedResults/FAQsResult.swift
+++ GithubJobs/Networking/FeedResults/FAQsResult.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct FAQsResult: Decodable {
+
+    let faqs: [FAQ]
+
+    init(faqs: [FAQ]) {
+        self.faqs = faqs
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        self.faqs = try container.decode([FAQ].self)
+    }
+
+}
