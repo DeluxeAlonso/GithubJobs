88996d7decc5c1412db036733f60c89cd0998fe5
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-12-09 13:01:07 -0500

    Implements FAQsCoordinator

--- GithubJobs/Scenes/Settings/FAQs/FAQsCoordinator.swift
+++ GithubJobs/Scenes/Settings/FAQs/FAQsCoordinator.swift
@@ -14,7 +14,24 @@ final class FAQsCoordinator: BaseCoordinator {
     var detailNavigationController: UINavigationController?
 
     override func start() {
-        // TODO
+        let faqsClient = FAQsClient()
+        let interactor = FAQsInteractor(faqsClient: faqsClient)
+        let viewModel = FAQsViewModel(interactor: interactor)
+        let view = FAQsContent(viewModel: viewModel)
+        let viewController = UIHostingController(rootView: view)
+        // TODO: - This should be updated from view model if possible
+        viewController.title = "FAQs"
+
+        if let detailNavigationController = detailNavigationController {
+            detailNavigationController.pushViewController(viewController, animated: false)
+            navigationController.showDetailViewController(detailNavigationController, sender: nil)
+        } else {
+            detailNavigationController = navigationController
+            navigationController.pushViewController(viewController, animated: true)
+        }
+        if navigationController.delegate == nil {
+            navigationController.delegate = self
+        }
     }
 
     func dismiss() {
