fb23c52878f47fdcda1f1229d55a798ffb623a49
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-12-08 23:51:24 -0500

    Adds TODO comment

--- GithubJobs/Scenes/Settings/FAQs/FAQsViewModel.swift
+++ GithubJobs/Scenes/Settings/FAQs/FAQsViewModel.swift
@@ -36,6 +36,7 @@ final class FAQsViewModel: FAQsViewModelProtocol {
         viewState = .loading
         switch await interactor.getAllFAQs() {
         case .success(let faqs):
+            // TODO: - Populate items
             self.viewState = .populated
         case .failure(let error):
             self.errorViewModel = ErrorViewModel(localizedError: error)
