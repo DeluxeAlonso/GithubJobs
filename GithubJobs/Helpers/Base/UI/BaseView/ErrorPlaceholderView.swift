cb544f69af52ee3c6aef9e436697531b6e34bc2c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-01-11 02:11:20 -0500

    Implements ErrorPlaceholderView

--- GithubJobs/Helpers/Base/UI/BaseView/ErrorPlaceholderView.swift
+++ GithubJobs/Helpers/Base/UI/BaseView/ErrorPlaceholderView.swift
@@ -5,4 +5,23 @@
 //  Created by Alonso on 11/01/25.
 //
 
-import Foundation
+import SwiftUI
+
+protocol ErrorPlaceholderView: View {
+    associatedtype ErrorContent: View
+
+    var error: ErrorContent { get }
+    var errorViewModel: ErrorViewModel? { get }
+}
+
+extension ErrorPlaceholderView {
+    var error: some View {
+        VStack {
+            Spacer()
+            errorViewModel.flatMap {
+                ErrorView(viewModel: $0)
+            }
+            Spacer()
+        }
+    }
+}
