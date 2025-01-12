dbcb9a3635f7d4f91c7b05646c537b1bc5fc6722
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-01-12 11:38:07 -0500

    Inherits BaseView from LoadableView and ErrorPlaceholderView protocols

--- GithubJobs/Helpers/Base/UI/BaseView/BaseView.swift
+++ GithubJobs/Helpers/Base/UI/BaseView/BaseView.swift
@@ -7,40 +7,12 @@
 
 import SwiftUI
 
-protocol BaseView: View {
+protocol BaseView: LoadableView, ErrorPlaceholderView {
     associatedtype MainContent: View
 
-    associatedtype LoadingContent: View
     associatedtype PopulatedContent: View
-    associatedtype ErrorContent: View
 
-    var loading: LoadingContent { get }
     var populated: PopulatedContent { get }
 
-    var error: ErrorContent { get }
-    var errorViewModel: ErrorViewModel? { get }
-
     var content: MainContent { get }
-}
-
-extension BaseView {
-    var loading: some View {
-        VStack {
-            ProgressView()
-                .progressViewStyle(CircularProgressViewStyle())
-                .controlSize(.large)
-                .padding(.top, 24.0)
-            Spacer()
-        }
-    }
-
-    var error: some View {
-        VStack {
-            Spacer()
-            errorViewModel.flatMap {
-                ErrorView(viewModel: $0)
-            }
-            Spacer()
-        }
-    }
 }
