dd1a71021903d7f99ae3460e5b786eb1129a997b
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-01-25 00:52:46 -0500

    Adds unit tests for computed properties in FAQsItemViewModel

--- GithubJobsTests/Settings/FAQs/FAQsItemViewModelTests.swift
+++ GithubJobsTests/Settings/FAQs/FAQsItemViewModelTests.swift
@@ -5,4 +5,29 @@
 //  Created by Alonso on 25/01/25.
 //
 
-import Foundation
+import XCTest
+import Combine
+@testable import GithubJobs
+
+@MainActor
+final class FAQsItemViewModelTests: XCTestCase {
+
+    func testPadding() {
+        // Arrange
+        let viewModel = FAQsItemViewModel(faq: .init(id: "", title: "", descriptions: []))
+        // Act
+        let padding = viewModel.padding
+        // Assert
+        XCTAssertEqual(padding, 16.0)
+    }
+
+    func testSubtitlesVerticalSpacing() {
+        // Arrange
+        let viewModel = FAQsItemViewModel(faq: .init(id: "", title: "", descriptions: []))
+        // Act
+        let subtitlesVerticalSpacing = viewModel.subtitlesVerticalSpacing
+        // Assert
+        XCTAssertEqual(subtitlesVerticalSpacing, 8.0)
+    }
+
+}
