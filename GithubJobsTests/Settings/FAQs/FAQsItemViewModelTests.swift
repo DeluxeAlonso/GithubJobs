82908eb39d6defa138d03e058eaa6b7a7fbf82b7
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-01-25 00:58:47 -0500

    Adds unit test for expandCollapseStyleConfiguration

--- GithubJobsTests/Settings/FAQs/FAQsItemViewModelTests.swift
+++ GithubJobsTests/Settings/FAQs/FAQsItemViewModelTests.swift
@@ -30,4 +30,18 @@ final class FAQsItemViewModelTests: XCTestCase {
         XCTAssertEqual(subtitlesVerticalSpacing, 8.0)
     }
 
+    func testExpandCollapseStyleConfiguration() {
+        // Arrange
+        let viewModel = FAQsItemViewModel(faq: .init(id: "", title: "", descriptions: []))
+        // Act
+        let expandCollapseStyleConfiguration = viewModel.expandCollapseStyleConfiguration
+        // Assert
+        XCTAssertEqual(expandCollapseStyleConfiguration.expandedIconName, "minus")
+        XCTAssertEqual(expandCollapseStyleConfiguration.collapsedIconName, "plus")
+        XCTAssertEqual(expandCollapseStyleConfiguration.iconSize, CGSize(width: 16, height: 16))
+        XCTAssertEqual(expandCollapseStyleConfiguration.iconTrailingPadding, 16)
+        XCTAssertEqual(expandCollapseStyleConfiguration.verticalSpacing, 8)
+        XCTAssertEqual(expandCollapseStyleConfiguration.horizontalSpacing, 8)
+    }
+
 }
