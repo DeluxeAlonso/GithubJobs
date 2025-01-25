3f30f917b12d4340e8cc536eb760b93be17ba997
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-01-25 00:48:53 -0500

    Creates FAQsItemViewModel file

--- GithubJobs/Scenes/Settings/FAQs/FAQsViewModel.swift
+++ GithubJobs/Scenes/Settings/FAQs/FAQsViewModel.swift
@@ -67,37 +67,3 @@ protocol FAQsItemViewModelProtocol: ObservableObject {
     var subtitlesVerticalSpacing: CGFloat { get }
 
 }
-
-final class FAQsItemViewModel: FAQsItemViewModelProtocol {
-
-    let title: String
-    let subtitles: [String]
-    @Published var expanded: Bool = false
-
-    var expandCollapseStyleConfiguration: ExpandCollapseControlStyleConfiguration {
-        ExpandCollapseControlStyleConfiguration(expandedIconName: "minus",
-                                                collapsedIconName: "plus",
-                                                iconSize: CGSize(width: 16.0, height: 16.0),
-                                                iconTrailingPadding: 16.0,
-                                                verticalSpacing: 8.0,
-                                                horizontalSpacing: 8.0)
-    }
-
-    var padding: CGFloat { 16.0 }
-
-    var subtitlesVerticalSpacing: CGFloat { 8.0 }
-
-    init(title: String,
-         subtitles: [String],
-         expanded: Bool = false) {
-        self.title = title
-        self.subtitles = subtitles
-        self.expanded = expanded
-    }
-
-    init(faq: FAQ) {
-        self.title = faq.title
-        self.subtitles = faq.descriptions
-        self.expanded = false
-    }
-}
