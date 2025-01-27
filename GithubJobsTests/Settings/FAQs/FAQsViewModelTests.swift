677ef3df426e18771ed8df17aba43245dcb454f9
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-01-26 23:35:41 -0500

    Adds missing unit tests for FAQsViewModel

--- GithubJobsTests/Settings/FAQs/FAQsViewModelTests.swift
+++ GithubJobsTests/Settings/FAQs/FAQsViewModelTests.swift
@@ -29,6 +29,64 @@ final class FAQsViewModelTests: XCTestCase {
         try super.tearDownWithError()
     }
 
+    func testLoadSuccess() async throws {
+        // Arrange
+        let faqsExpectation = expectation(description: "We should retrieve FAQs models")
+        let stateExpectation = expectation(description: "State is set to populated")
+        let faqs = [
+            FAQ(id: "ID", title: "Title", descriptions: ["Description"]),
+            FAQ(id: "ID2", title: "Title2", descriptions: ["Description2"])
+        ]
+        mockInteractor.getAllFAQsResult = .success(faqs)
+        // Act
+        viewModel.$items
+            .dropFirst()
+            .sink { faqs in
+                XCTAssertEqual(faqs.count, 2)
+                faqsExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        viewModel.$viewState
+            .dropFirst()
+            .sink { state in
+                XCTAssertEqual(state, .populated)
+                faqsExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        await viewModel.load()
+        // Assert
+        await fulfillment(of: [faqsExpectation, stateExpectation], timeout: 1.0)
+    }
+
+    func testLoadError() async throws {
+        // Arrange
+        let faqsExpectation = expectation(description: "We should retrieve FAQs models")
+        let stateExpectation = expectation(description: "State is set to populated")
+        let faqs = [
+            FAQ(id: "ID", title: "Title", descriptions: ["Description"]),
+            FAQ(id: "ID2", title: "Title2", descriptions: ["Description2"])
+        ]
+        mockInteractor.getAllFAQsResult = .success(faqs)
+        // Act
+        viewModel.$items
+            .dropFirst()
+            .sink { faqs in
+                XCTAssertEqual(faqs.count, 2)
+                faqsExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        viewModel.$viewState
+            .dropFirst()
+            .sink { state in
+                XCTAssertEqual(state, .populated)
+                faqsExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        await viewModel.load()
+        // Assert
+        await fulfillment(of: [faqsExpectation, stateExpectation], timeout: 1.0)
+    }
+
     func testVerticalSpacing() {
         // Act
         let verticalSpacing = viewModel.verticalSpacing
