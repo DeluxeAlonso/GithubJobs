72e65e42a1584adbb70bb2a0f81795b967979bba
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-01-30 01:21:35 -0500

    Implements MockFeatureFlagsInteractor

--- GithubJobsTests/Mocks/MockFeatureFlagsInteractor.swift
+++ GithubJobsTests/Mocks/MockFeatureFlagsInteractor.swift
@@ -7,7 +7,18 @@
 
 @testable import GithubJobs
 
-final class MockFeatureFlagsInteractor: FeatureFlagsInteractor {
+final class MockFeatureFlagsInteractor: FeatureFlagsInteractorProtocol {
+
+    var getAllFeatureFlagsResult: Result<[FeatureFlagProtocol], APIError> = .success([])
+    private(set) var getAllFeatureFlagsCallCount = 0
+    func getAllFeatureFlags() async -> Result<[FeatureFlagProtocol], APIError> {
+        getAllFeatureFlagsCallCount += 1
+        return getAllFeatureFlagsResult
+    }
+
+    private(set) var updateFeatureFlagCallCount = 0
+    func updateFeatureFlag(identifier: String, value: Bool) async {
+        updateFeatureFlagCallCount += 1
+    }
 
 }
-
