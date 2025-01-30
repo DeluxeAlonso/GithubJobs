461a04f26ea0dd1aa7b5c23fbefecbf5e1eb1275
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-01-30 01:12:27 -0500

    Creates MockFeatureFlagsInteractor

---
+++ GithubJobsTests/Mocks/MockFeatureFlagsInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  MockFeatureFlagsInteractor.swift
+//  GithubJobsTests
+//
+//  Created by Alonso on 30/01/25.
+//
+
+@testable import GithubJobs
+
+final class MockFeatureFlagsInteractor: FeatureFlagsInteractor {
+
+}
+
