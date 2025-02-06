d2f24b39d27df605a7763e25bb6d3510305e02bc
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-02-06 17:10:43 -0500

    Updates access levels in ViewController

--- GithubJobs/Helpers/Base/UI/SplitViewController.swift
+++ GithubJobs/Helpers/Base/UI/SplitViewController.swift
@@ -12,7 +12,7 @@ class SplitViewController: UISplitViewController, Themeable {
 
     private let themeManager: ThemeManagerProtocol
 
-    private(set) var cancellables: Set<AnyCancellable> = []
+    var cancellables: Set<AnyCancellable> = []
 
     // MARK: - Initializers
 
