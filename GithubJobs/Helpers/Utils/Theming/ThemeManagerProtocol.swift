a54ed249b436532da6f02bcb30b1e7895ab5d789
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-02-12 23:52:05 -0500

    Updates ThemeManagerProtocol

--- GithubJobs/Helpers/Utils/Theming/ThemeManagerProtocol.swift
+++ GithubJobs/Helpers/Utils/Theming/ThemeManagerProtocol.swift
@@ -10,6 +10,10 @@ import Combine
 
 protocol ThemeManagerProtocol: AnyObject {
 
+    var theme: CurrentValueSubject<Theme, Never> { get }
+
+    func updateTheme(_ theme: Theme)
+
     var interfaceStyle: CurrentValueSubject<UIUserInterfaceStyle, Never> { get }
 
     func updateInterfaceStyle(_ userInterfaceStyle: UIUserInterfaceStyle)
