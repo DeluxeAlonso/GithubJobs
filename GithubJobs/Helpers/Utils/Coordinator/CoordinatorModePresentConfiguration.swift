7e3159c522a82612a4474e27079ac78e4e571de1
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-02-20 23:53:22 -0500

    Implements CoordinatorModePresentConfiguration

---
+++ GithubJobs/Helpers/Utils/Coordinator/CoordinatorModePresentConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  CoordinatorModePresentConfiguration.swift
+//  GithubJobs
+//
+//  Created by Alonso on 20/02/25.
+//
+
+import UIKit
+
+struct CoordinatorModePresentConfiguration {
+    let modalPresentationStyle: UIModalPresentationStyle
+    let transitioningDelegate: UIViewControllerTransitioningDelegate?
+}
