ed88f89f741779de08bc33b692056e32a924884b
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-02-26 23:47:43 -0500

    Fixes compile errors

--- GithubJobs/SceneDelegate.swift
+++ GithubJobs/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var mainCoordinator: CoordinatorDEPRECATED!
+    var mainCoordinator: Coordinator!
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowsScene = (scene as? UIWindowScene) else { return }
