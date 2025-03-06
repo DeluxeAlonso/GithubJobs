6afd90f121b8a2d3c5c360d1509b505f09adba44
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-03-05 23:37:10 -0500

    Updates ErrorViewModel

--- GithubJobs/Helpers/ViewComponents/ErrorView/ErrorViewModel.swift
+++ GithubJobs/Helpers/ViewComponents/ErrorView/ErrorViewModel.swift
@@ -11,6 +11,7 @@ protocol ErrorViewModelProtocol {
 
     var title: String? { get }
     var subtitles: [String] { get }
+    var imageSize: CGSize { get }
 
 }
 
@@ -18,6 +19,10 @@ struct ErrorViewModel: ErrorViewModelProtocol {
 
     let title: String?
     let subtitles: [String]
+
+    var imageSize: CGSize {
+        CGSize(width: 64.0, height: 64.0)
+    }
 
     init(title: String? = nil, subtitles: [String] = []) {
         self.title = title
