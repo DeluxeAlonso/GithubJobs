5b4b82033b8a29d64e46c941fa9cd12ee45b6057
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-03-08 00:20:57 -0500

    Implements FAQResult networking model

---
+++ GithubJobs/Networking/FeedResults/FAQs/FAQResult.swift
@@ -0,0 +1,16 @@
+//
+//  FAQResult.swift
+//  GithubJobs
+//
+//  Created by Alonso on 8/03/25.
+//
+
+import Foundation
+
+struct FAQResult: Decodable {
+
+    let id: String
+    let title: String
+    let descriptions: [String]
+
+}
