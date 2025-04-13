ca81b8d7112bb83d48ddc104d451201c99edb1dc
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-04-13 00:53:39 -0500

    Implements ThemeSelectionInteractor

--- GithubJobs/Scenes/Settings/ThemeSelection/ThemeSelectionInteractor.swift
+++ GithubJobs/Scenes/Settings/ThemeSelection/ThemeSelectionInteractor.swift
@@ -6,5 +6,24 @@
 //
 
 final class ThemeSelectionInteractor: ThemeSelectionInteractorProtocol {
+
+    private let themeManager: ThemeManagerProtocol
+
+    init(themeManager: ThemeManagerProtocol) {
+        self.themeManager = themeManager
+    }
+
+    func getAllThemes() async throws -> [ThemeSelectionItemModel] {
+        let selectedTheme = await themeManager.theme
+        return Theme.allCases.map { theme in
+            let isSelected = selectedTheme == theme
+            return ThemeSelectionItemModel(theme, isSelected: isSelected)
+        }
+    }
     
+    func updateTheme(_ theme: Theme) async throws -> [ThemeSelectionItemModel] {
+        await themeManager.updateTheme(theme)
+        return try await getAllThemes()
+    }
+
 }
