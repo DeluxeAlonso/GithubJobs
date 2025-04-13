f27891e115a87cd9c0b77827a9e20aabf3fe3c0f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-04-13 00:51:19 -0500

    Creates ThemeSelectionInteractor

---
+++ GithubJobs/Scenes/Settings/ThemeSelection/ThemeSelectionInteractor.swift
@@ -0,0 +1,10 @@
+//
+//  ThemeSelectionInteractor.swift
+//  GithubJobs
+//
+//  Created by Alonso on 13/04/25.
+//
+
+final class ThemeSelectionInteractor: ThemeSelectionInteractorProtocol {
+    
+}
