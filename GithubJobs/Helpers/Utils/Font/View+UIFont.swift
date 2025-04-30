3be277229048e41629b112530871de2d54fd9ca8
DeluxeAlonso <alonso.alvarez@pucp.pe>
2025-04-30 00:33:43 -0500

    Implements UIFont modifier

--- GithubJobs/Helpers/Utils/Font/View+UIFont.swift
+++ GithubJobs/Helpers/Utils/Font/View+UIFont.swift
@@ -5,3 +5,13 @@
 //  Created by Alonso on 30/04/25.
 //
 
+import SwiftUI
+
+extension View {
+
+    /// Applies a UIFont to a SwiftUI view
+    func uiFont(_ font: UIFont) -> some View {
+        self.font(Font(font))
+    }
+
+}
